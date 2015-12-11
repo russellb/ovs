f9ac1ad5259126d1de700bd29f58a82d7a964317
Russell Bryant <russell@ovn.org>
2015-12-11 16:15:10 -0500

    python: Remove unused imports and variables.
    Signed-off-by: Russell Bryant <russell@ovn.org>

--- python/ovs/socket_util.py
+++ python/ovs/socket_util.py
@@ -16,7 +16,6 @@ import errno
 import os
 import os.path
 import random
-import select
 import socket
 import sys
 
@@ -302,12 +301,12 @@ def set_dscp(sock, family, dscp):
     if family == socket.AF_INET:
         try:
             sock.setsockopt(socket.IPPROTO_IP, socket.IP_TOS, val)
-        except socket.error as e:
+        except socket.error:
             raise
     elif family == socket.AF_INET6:
         try:
             sock.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_TCLASS, val)
-        except socket.error as e:
+        except socket.error:
             raise
     else:
         raise
