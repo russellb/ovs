61c09e81856a8fed720daee3bc586a50b63ddddd
Russell Bryant <russell@ovn.org>
2015-12-12 13:09:44 -0500

    python: Restrict line length to 79 chars.
    Resolve pep8 error:
    
      E501 line too long (80 > 79 characters)
    
    Signed-off-by: Russell Bryant <russell@ovn.org>

--- python/ovs/dirs.py
+++ python/ovs/dirs.py
@@ -1,8 +1,8 @@
 import os
-PKGDATADIR = os.environ.get("OVS_PKGDATADIR", """/usr/local/share/openvswitch""")
-RUNDIR = os.environ.get("OVS_RUNDIR", """/var/run""")
-LOGDIR = os.environ.get("OVS_LOGDIR", """/usr/local/var/log""")
-BINDIR = os.environ.get("OVS_BINDIR", """/usr/local/bin""")
+PKGDATADIR = os.environ.get("OVS_PKGDATADIR", "/usr/local/share/openvswitch")
+RUNDIR = os.environ.get("OVS_RUNDIR", "/var/run")
+LOGDIR = os.environ.get("OVS_LOGDIR", "/usr/local/var/log")
+BINDIR = os.environ.get("OVS_BINDIR", "/usr/local/bin")
 
 DBDIR = os.environ.get("OVS_DBDIR")
 if not DBDIR:
@@ -10,4 +10,4 @@ if not DBDIR:
     if sysconfdir:
         DBDIR = "%s/openvswitch" % sysconfdir
     else:
-        DBDIR = """/usr/local/etc/openvswitch"""
+        DBDIR = "/usr/local/etc/openvswitch"
