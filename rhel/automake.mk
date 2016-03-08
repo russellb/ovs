russellb/ovs

commit 8520deefbf208b24518c2bf67baad6d230f39dc0

Use 'RUNDIR' from make for rhel/ovn-controller.service
Perviously it was using the platform's runtime directory which can be
different from the runtime directory of ovsdb-server started by the
openvswitch service

Signed-off-by: Babu Shanmugam <bschanmu@redhat.com>
Acked-by: Flavio Leitner <fbl@sysclose.org>
Signed-off-by: Russell Bryant <russell@ovn.org>

rhel/.gitignore
@@ -4,3 +4,4 @@ openvswitch-kmod-rhel6.spec
 openvswitch-kmod-fedora.spec
 openvswitch.spec
 openvswitch-fedora.spec
+usr_lib_systemd_system_ovn-controller.service

rhel/automake.mk
@@ -28,6 +28,7 @@ EXTRA_DIST += \
 	rhel/usr_lib_systemd_system_openvswitch.service \
 	rhel/usr_lib_systemd_system_openvswitch-nonetwork.service \
 	rhel/usr_lib_systemd_system_ovn-controller.service \
+	rhel/usr_lib_systemd_system_ovn-controller.service.in \
 	rhel/usr_lib_systemd_system_ovn-controller-vtep.service \
 	rhel/usr_lib_systemd_system_ovn-northd.service
 
@@ -50,6 +51,8 @@ $(srcdir)/rhel/openvswitch.spec: rhel/openvswitch.spec.in $(top_builddir)/config
 
 $(srcdir)/rhel/openvswitch-fedora.spec: rhel/openvswitch-fedora.spec.in $(top_builddir)/config.status
 	$(update_rhel_spec)
+
+$(srcdir)/rhel/usr_lib_systemd_system_ovn-controller.service: rhel/usr_lib_systemd_system_ovn-controller.service.in $(top_builddir)/config.status
 
 RPMBUILD_TOP := $(abs_top_builddir)/rpm/rpmbuild
 

rhel/usr_lib_systemd_system_ovn-controller.service.in
@@ -15,8 +15,8 @@ After=openvswitch.service
 
 [Service]
 Type=simple
-Environment=OVS_RUNDIR=%t/openvswitch
-Environment=OVS_DB=unix:%t/openvswitch/db.sock
+Environment=OVS_RUNDIR=@RUNDIR@
+Environment=OVS_DB=unix:@RUNDIR@/db.sock
 ExecStart=/usr/bin/ovn-controller -vconsole:emer -vsyslog:err -vfile:info \
           --log-file=/var/log/openvswitch/ovn-controller.log \
           --no-chdir --pidfile=${OVS_RUNDIR}/ovn-controller.pid ${OVS_DB}

commit 9863bb9868563ab70e8ee481668181d4c9e58a2a

rhel: Use ovn-ctl to ovn-controller service
Trying to revert the changes caused by 8520deefbf208b24518c2bf67baad6d230f39dc0
to travis build failures

Signed-off-by: Babu Shanmugam <bschanmu@redhat.com>

rhel/.gitignore
@@ -4,4 +4,3 @@ openvswitch-kmod-rhel6.spec
 openvswitch-kmod-fedora.spec
 openvswitch.spec
 openvswitch-fedora.spec
-usr_lib_systemd_system_ovn-controller.service

rhel/automake.mk
@@ -28,7 +28,6 @@ EXTRA_DIST += \
 	rhel/usr_lib_systemd_system_openvswitch.service \
 	rhel/usr_lib_systemd_system_openvswitch-nonetwork.service \
 	rhel/usr_lib_systemd_system_ovn-controller.service \
-	rhel/usr_lib_systemd_system_ovn-controller.service.in \
 	rhel/usr_lib_systemd_system_ovn-controller-vtep.service \
 	rhel/usr_lib_systemd_system_ovn-northd.service
 
@@ -51,8 +50,6 @@ $(srcdir)/rhel/openvswitch.spec: rhel/openvswitch.spec.in $(top_builddir)/config
 
 $(srcdir)/rhel/openvswitch-fedora.spec: rhel/openvswitch-fedora.spec.in $(top_builddir)/config.status
 	$(update_rhel_spec)
-
-$(srcdir)/rhel/usr_lib_systemd_system_ovn-controller.service: rhel/usr_lib_systemd_system_ovn-controller.service.in $(top_builddir)/config.status
 
 RPMBUILD_TOP := $(abs_top_builddir)/rpm/rpmbuild
 

rhel/usr_lib_systemd_system_ovn-controller.service
@@ -14,9 +14,7 @@ Requires=openvswitch.service
 After=openvswitch.service
 
 [Service]
-Type=simple
-Environment=OVS_RUNDIR=@RUNDIR@
-Environment=OVS_DB=unix:@RUNDIR@/db.sock
-ExecStart=/usr/bin/ovn-controller -vconsole:emer -vsyslog:err -vfile:info \
-          --log-file=/var/log/openvswitch/ovn-controller.log \
-          --no-chdir --pidfile=${OVS_RUNDIR}/ovn-controller.pid ${OVS_DB}
+Type=oneshot
+RemainAfterExit=yes
+ExecStart=/usr/share/openvswitch/scripts/ovn-ctl start_controller
+ExecStop=/usr/share/openvswitch/scripts/ovn-ctl stop_controller